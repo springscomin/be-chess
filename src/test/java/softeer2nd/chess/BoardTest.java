cf990e1ecfa51198bb96d20195328e2419704ba4
윤상현 <yshyeon102@gmail.com>
2023-07-04 22:38:55 +0900

    test(BoardTest): 요구사항을 반영한 테스트 코드 작성

--- src/test/java/softeer2nd/chess/BoardTest.java
+++ src/test/java/softeer2nd/chess/BoardTest.java
@@ -1,5 +1,17 @@
 package softeer2nd.chess;
 
-public class BoardTest {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BoardTest {
+    @Test
+    public void initialize() throws Exception {
+        Board board = new Board();
+
+        board.initialize();
+
+        assertEquals("pppppppp", board.getWhitePawnsResult());
+        assertEquals("PPPPPPPP", board.getBlackPawnsResult());
+    }
 }
