c10596acda1f434fe583b0b9bd435e8063bc7ed9
윤상현 <yshyeon102@gmail.com>
2023-07-04 23:02:07 +0900

    feat(Board): Board 클래스 기능 추가
    - initialize() : 체스판 초기화 메서드
    - print() : 체스 보드 판 출력 메서드
    - getBlackPawnResult()  
    - getWhitePawnResult()

--- src/main/java/softeer2nd/chess/Board.java
+++ src/main/java/softeer2nd/chess/Board.java
@@ -4,23 +4,78 @@ import softeer2nd.chess.pieces.Pawn;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Board {
-    private final List<Pawn> boards;
+    public static final char LINE_BREAK = '\n';
+    public static final char EMPTY_REPRESENTATION = '.';
 
-    public Board() {
-        this.boards = new ArrayList<>();
+    public static final int BLACK_PAWN_INIT_LINE = 1;
+    public static final int WHITE_PAWN_INIT_LINE = 6;
+
+    public static final int BOARD_LENGTH = 8;
+
+    private List<List<Pawn>> boards;
+
+    public void initialize() {
+        initializeEmptyBoard();
+        addWhitePawn();
+        addBlackPawn();
     }
 
-    public void add(Pawn pawn) {
-        boards.add(pawn);
+    public String getWhitePawnsResult() {
+        List<Pawn> whitePawns = boards.get(WHITE_PAWN_INIT_LINE);
+        return makeLineRepresentation(whitePawns);
     }
 
-    public Pawn findPawn(int position) {
-        return boards.get(position);
+    public String getBlackPawnsResult() {
+        List<Pawn> blackPawns = boards.get(BLACK_PAWN_INIT_LINE);
+        return makeLineRepresentation(blackPawns);
     }
 
-    public int size() {
-        return boards.size();
+    public String print() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (List<Pawn> line : boards) {
+            String lineRepresentation = makeLineRepresentation(line);
+            stringBuilder.append(lineRepresentation).append(LINE_BREAK);
+        }
+
+        return stringBuilder.toString();
+    }
+
+
+    private String makeLineRepresentation(List<Pawn> pawns) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Pawn pawn : pawns) {
+            char representation = getRepresentation(pawn);
+            stringBuilder.append(representation);
+        }
+        return stringBuilder.toString();
+    }
+
+    private char getRepresentation(Pawn pawn) {
+        char representation = EMPTY_REPRESENTATION;
+        if (!Objects.isNull(pawn)) representation = pawn.getRepresentation();
+        return representation;
+    }
+
+    private void initializeEmptyBoard() {
+        boards = new ArrayList<>();
+        for (int row = 0; row < BOARD_LENGTH; row++) {
+            ArrayList<Pawn> pawns = new ArrayList<>();
+            for (int col = 0; col < BOARD_LENGTH; col++) pawns.add(null);
+            boards.add(pawns);
+        }
+    }
+
+    private void addWhitePawn() {
+        List<Pawn> whitePawns = boards.get(WHITE_PAWN_INIT_LINE);
+        whitePawns.replaceAll(pawn -> new Pawn(Pawn.WHITE, Pawn.WHITE_REPRESENTATION));
+    }
+
+    private void addBlackPawn() {
+        List<Pawn> blackPawns = boards.get(BLACK_PAWN_INIT_LINE);
+        blackPawns.replaceAll(pawn -> new Pawn(Pawn.BLACK, Pawn.BLACK_REPRESENTATION));
     }
 }
