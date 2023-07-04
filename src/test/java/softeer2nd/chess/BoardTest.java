f9da97c97562b1d571c41a6617bbdda1e9fa3c1f
윤상현 <yshyeon102@gmail.com>
2023-07-04 13:32:12 +0900

    representation 변수 추가에 따른 테스트 코드 수정

--- src/test/java/softeer2nd/chess/BoardTest.java
+++ src/test/java/softeer2nd/chess/BoardTest.java
@@ -10,12 +10,12 @@ public class BoardTest {
     public void create() throws Exception {
         Board board = new Board();
 
-        Pawn white = new Pawn(Pawn.WHITE);
+        Pawn white = new Pawn(Pawn.WHITE, Pawn.WHITE_REPRESENTATION);
         board.add(white);
         assertEquals(1, board.size());
         assertEquals(white, board.findPawn(0));
 
-        Pawn black = new Pawn(Pawn.BLACK);
+        Pawn black = new Pawn(Pawn.BLACK, Pawn.BLACK_REPRESENTATION);
         board.add(black);
         assertEquals(2, board.size());
         assertEquals(black, board.findPawn(1));
