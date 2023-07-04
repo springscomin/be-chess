cda3e7a77610f218b69169b0af3b56c0bb8c525d
윤상현 <yshyeon102@gmail.com>
2023-07-04 22:37:47 +0900

    test(BoardTest): 요구사향 변경에 따른 테스트 코드 삭제

--- src/test/java/softeer2nd/chess/BoardTest.java
+++ src/test/java/softeer2nd/chess/BoardTest.java
@@ -1,23 +1,5 @@
 package softeer2nd.chess;
 
-import org.junit.jupiter.api.Test;
-import softeer2nd.chess.pieces.Pawn;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class BoardTest {
-    @Test
-    public void create() throws Exception {
-        Board board = new Board();
 
-        Pawn white = new Pawn(Pawn.WHITE, Pawn.WHITE_REPRESENTATION);
-        board.add(white);
-        assertEquals(1, board.size());
-        assertEquals(white, board.findPawn(0));
-
-        Pawn black = new Pawn(Pawn.BLACK, Pawn.BLACK_REPRESENTATION);
-        board.add(black);
-        assertEquals(2, board.size());
-        assertEquals(black, board.findPawn(1));
-    }
 }
