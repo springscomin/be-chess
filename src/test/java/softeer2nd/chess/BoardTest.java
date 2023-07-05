05448a7c07b73b687eee033252cbf17ce56c8ada
윤상현 <yshyeon102@gmail.com>
2023-07-05 17:22:20 +0900

    test(BoardTest): 모든 기물들이 추가된 보드판 출력 테스트 작성

--- src/test/java/softeer2nd/chess/BoardTest.java
+++ src/test/java/softeer2nd/chess/BoardTest.java
@@ -1,10 +1,20 @@
 package softeer2nd.chess;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static softeer2nd.utils.StringUtils.appendNewLine;
 
 public class BoardTest {
+
+    private Board board;
+
+    @BeforeEach
+    public void setup() {
+        board = new Board();
+    }
+
     @Test
     public void initialize() throws Exception {
         Board board = new Board();
@@ -13,5 +23,20 @@ public class BoardTest {
 
         assertEquals("pppppppp", board.getWhitePawnsResult());
         assertEquals("PPPPPPPP", board.getBlackPawnsResult());
+    }
+
+    @Test
+    public void create() throws Exception {
+        board.initialize();
+        assertEquals(32, board.pieceCount());
+        String blankRank = appendNewLine("........");
+        assertEquals(
+                appendNewLine("RNBQKBNR") +
+                appendNewLine("PPPPPPPP") +
+                blankRank + blankRank + blankRank + blankRank +
+                appendNewLine("pppppppp") +
+                appendNewLine("rnbqkbnr"),
+                board.showBoard()
+        );
     }
 }
