0b7a3e29a89f8bf9bc3e20aec7953346fda3f7bc
윤상현 <yshyeon102@gmail.com>
2023-07-06 11:42:40 +0900

    feat(PieceType): 기물 종류에 따른 점수 속성 추가

--- src/main/java/softeer2nd/chess/pieces/PieceType.java
+++ src/main/java/softeer2nd/chess/pieces/PieceType.java
@@ -1,18 +1,25 @@
 package softeer2nd.chess.pieces;
 
 public enum PieceType {
-    PAWN('p'),
-    KNIGHT('n'),
-    ROOK('r'),
-    BISHOP('b'),
-    QUEEN('q'),
-    KING('k'),
-    NO_PIECE('.');
+    PAWN('p', 1.0),
+    ROOK('r', 5.0),
+    KNIGHT('n', 2.5),
+    BISHOP('b', 3.0),
+    QUEEN('q', 9.0),
+    KING('k', 0.0),
+    NO_PIECE('.', 0.0);
 
     private final char representation;
 
-    PieceType(char representation) {
+    private final double defaultPoint;
+
+    PieceType(char representation, double defaultPoint) {
         this.representation = representation;
+        this.defaultPoint = defaultPoint;
+    }
+
+    public double getDefaultPoint() {
+        return defaultPoint;
     }
 
     public char getDefaultRepresentation() {
