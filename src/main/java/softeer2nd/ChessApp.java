10f8af4b1c46b9b54494ed94aba6a49f37354cec
윤상현 <yshyeon102@gmail.com>
2023-07-13 00:57:12 +0900

    feat(ChessApp): 게임이 진행중인지 확인하는 기능 추가

--- src/main/java/softeer2nd/ChessApp.java
+++ src/main/java/softeer2nd/ChessApp.java
@@ -41,12 +41,19 @@ public class ChessApp {
     }
 
     private void move(Command command) {
-        assert game != null;
+        verifyGameStarted();
         String[] args = command.getArgs();
         game.movePiece(args[0], args[1]);
     }
 
-    private static void exitGame() {
+    private void verifyGameStarted() {
+        if (game == null) {
+            throw new RuntimeException("Game이 진행되지 않았습니다.");
+        }
+    }
+
+    private void exitGame() {
+        verifyGameStarted();
         System.exit(0);
     }
 }
